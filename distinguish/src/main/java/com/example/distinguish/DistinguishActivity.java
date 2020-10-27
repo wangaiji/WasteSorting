07a533c15af1260d944d8778984242d7a861c1d7
wangaiji <1670142221@qq.com>
2020-10-27 17:35:03 +0800

    组件化

---
+++ distinguish/src/main/java/com/example/distinguish/DistinguishActivity.java
@@ -0,0 +1,15 @@
+package com.example.distinguish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DistinguishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distinguish);
+    }
+}
